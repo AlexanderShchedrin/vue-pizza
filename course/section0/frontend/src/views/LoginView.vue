caf8b2bf41adc117fc5575cd97da63300a9d6b54
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-08-07 17:12:31 +0300

    [3.23] - Добавил вёрстку всех страниц

--- course/section0/frontend/src/views/LoginView.vue
+++ course/section0/frontend/src/views/LoginView.vue
@@ -1,90 +1,71 @@
 <template>
-  <div class="modal">
-    <div class="modal__wrapper">
-      <div class="sign-form">
-        <a
-          href="#"
-          class="close close--white"
-          tabindex="0"
-          ref="dialog"
-          @click.self="closeDialog"
-          @keydown.esc="closeDialog"
-        >
-          <span class="visually-hidden">Закрыть форму авторизации</span>
-        </a>
-        <div class="sign-form__body">
-          <div class="sign-form__title">
-            <h1 class="title title--small">Авторизуйтесь на сайте</h1>
-          </div>
-          <form action="test.html" method="post">
-            <div class="sign-form__input">
-              <label class="input">
-                <span>E-mail</span>
-                <input type="email" name="email" placeholder="example@mail.ru">
-              </label>
-            </div>
-
-            <div class="sign-form__input">
-              <label class="input">
-                <span>Пароль</span>
-                <input type="password" name="pass" placeholder="***********">
-              </label>
-            </div>
-            <button type="submit" class="button">Авторизоваться</button>
-          </form>
-        </div>
-      </div>
+  <div class="sign-form">
+    <router-link :to="{ name: 'home' }" class="close close--white">
+      <span class="visually-hidden">Закрыть форму авторизации</span>
+    </router-link>
+    <div class="sign-form__title">
+      <h1 class="title title--small">Авторизуйтесь на сайте</h1>
     </div>
+    <form action="#" method="post">
+      <div class="sign-form__input">
+        <label class="input">
+          <span>E-mail</span>
+          <input
+            v-model="email"
+            type="email"
+            name="email"
+            placeholder="example@mail.ru"
+          />
+        </label>
+      </div>
+
+      <div class="sign-form__input">
+        <label class="input">
+          <span>Пароль</span>
+          <input
+            v-model="password"
+            type="password"
+            name="pass"
+            placeholder="***********"
+          />
+        </label>
+      </div>
+      <button type="submit" class="button">Авторизоваться</button>
+    </form>
   </div>
 </template>
+
 <script setup>
+import { ref } from "vue";
 
-import {useRoute, useRouter} from 'vue-router';
-import {onMounted, ref} from 'vue';
-
-const router = useRouter()
-const route = useRoute()
-
-const dialog = ref(null)
-
-onMounted(() => {
-  // Фокусируем на диалоговом окне чтобы сработала клавиша esc без дополнительного клика на окне
-  dialog.value.focus()
-})
-
-const closeDialog = function () {
-  router.push('/')
-}
-
+const email = ref("");
+const password = ref("");
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @import "@/assets/scss/ds-system/ds.scss";
 @import "@/assets/scss/mixins/mixins.scss";
-@import "@/assets/scss/modal";
-
-.content {
-  padding-top: 20px;
-}
 
 .sign-form {
+  @include pf_center-all;
+
+  z-index: 10;
+
   display: block;
+
   box-sizing: border-box;
+  width: 455px;
+  padding-top: 146px;
+  padding-right: 32px;
+  padding-bottom: 32px;
+  padding-left: 32px;
 
   background: $white url("@/assets/img/popup.svg") no-repeat center top;
-  background-size: 100%;
   box-shadow: $shadow-light;
 
   button {
     margin: 0 auto;
     padding: 16px 14px;
   }
-}
-
-.sign-form__body {
-  padding-top: 146px;
-  padding-right: 32px;
-  padding-bottom: 32px;
-  padding-left: 32px;
 }
 
 .sign-form__title {
@@ -97,4 +78,65 @@ const closeDialog = function () {
   margin-bottom: 16px;
 }
 
+.close {
+  position: absolute;
+  top: 16px;
+  right: 16px;
+
+  width: 25px;
+  height: 25px;
+
+  cursor: pointer;
+  transition: 0.3s;
+  text-decoration: none;
+
+  color: $black;
+  border-radius: 50%;
+  outline: none;
+
+  &::before,
+  &::after {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    width: 25px;
+    height: 2px;
+
+    content: "";
+
+    border-radius: 2px;
+    background-color: $black;
+  }
+
+  &::before {
+    transform: translate(-50%, -50%) rotate(-45deg);
+  }
+
+  &::after {
+    transform: translate(-50%, -50%) rotate(45deg);
+  }
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    opacity: 0.5;
+  }
+
+  &:focus {
+    &::before,
+    &::after {
+      background-color: $orange-100;
+    }
+  }
+
+  &--white {
+    &::before,
+    &::after {
+      background-color: $white;
+    }
+  }
+}
 </style>
