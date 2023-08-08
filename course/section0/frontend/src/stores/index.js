c4aca50db1786fac94ac58b7432bef793e352959
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-08-08 09:57:10 +0300

    [4.8] - Добавил хранилища

---
+++ course/section0/frontend/src/stores/index.js
@@ -0,0 +1,4 @@
+export { useDataStore } from '@/stores/data';
+export { useCartStore } from '@/stores/cart';
+export { usePizzaStore } from '@/stores/pizza';
+export { useProfileStore } from '@/stores/profile';
