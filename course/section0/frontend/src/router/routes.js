070a8b4672ff67b945d88f61a38137bd2b11debe
AlexanderShchedrin <shchedrin@intervolga.ru>
2023-08-14 12:18:55 +0300

    [5.13] - аутентификация и авторизация

--- course/section0/frontend/src/router/routes.js
+++ course/section0/frontend/src/router/routes.js
@@ -1,39 +1,25 @@
-export default [
+import { isLoggedIn } from '@/middlewares/isLoggedIn';
+
+export const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: () => import('@/views/HomeView.vue'),
-    meta: { layout: 'DefaultLayout' },
+    path: "",
+    name: "home",
+    component: () => import("@/views/HomeView.vue"),
+    meta: { layout: "DefaultLayout" },
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/LoginView.vue'),
-    meta: { layout: 'SimpleLayout' },
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/LoginView.vue"),
+    meta: { layout: "SimpleLayout" },
   },
   {
-    path: '/cart',
-    name: 'cart',
-    component: () => import('@/views/CartView.vue'),
-    meta: { layout: 'DefaultLayout' },
-  },
-  {
-    path: '/user',
-    name: 'user',
-    component: () => import('@/views/UserView.vue'),
-    meta: { layout: 'DefaultLayout' },
-    children: [
-      {
-        path: 'orders',
-        name: 'orders',
-        component: () => import('@/views/OrdersView.vue'),
-      },
-      {
-        path: 'profile',
-        name: 'profile',
-        component: () => import('@/views/ProfileView.vue'),
-      },
-    ],
+    path: "/cart",
+    name: "cart",
+    component: () => import("@/views/CartView.vue"),
+    meta: {
+      layout: "DefaultLayout",
+    },
   },
   {
     path: "/success",
@@ -41,4 +27,25 @@ export default [
     component: () => import("@/views/SuccessView.vue"),
     meta: { layout: "SimpleLayout" },
   },
-]
+  {
+    path: "/user",
+    name: "user",
+    component: () => import("@/views/UserView.vue"),
+    meta: {
+      layout: "DefaultLayout",
+      middlewares: [isLoggedIn],
+    },
+    children: [
+      {
+        path: "orders",
+        name: "orders",
+        component: () => import("@/views/OrdersView.vue"),
+      },
+      {
+        path: "profile",
+        name: "profile",
+        component: () => import("@/views/ProfileView.vue"),
+      },
+    ],
+  },
+];
--- course/section0/frontend/src/services/resources/index.js
+++ course/section0/frontend/src/services/resources/index.js
@@ -5,10 +5,12 @@ import { MiscResource } from "@/services/resources/misc.resource";
 import { OrderResource } from "@/services/resources/order.resource";
 import { SauceResource } from "@/services/resources/sauce.resource";
 import { SizeResource } from "@/services/resources/size.resource";
+import { AuthService } from '@/services/resources/auth.service';
 
 export default {
   address: new AddressResource(),
   dough: new DoughResource(),
+  auth: new AuthService(),
   ingredient: new IngredientResource(),
   misc: new MiscResource(),
   order: new OrderResource(),
